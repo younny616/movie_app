ffc1b71d2e1c5f9b4b558c4ee001c44fe6c417f3
yoonnayoung <younny616@gmail.com>
2020-12-25 17:39:57 +0900

    #3.1 All you need to know about State

--- src/App.js
+++ src/App.js
@@ -7,10 +7,13 @@ class App extends React.Component{
     count: 0
   }
   add = () => {
-    console.log("add");
+    // setState 를 호출하면 react는 state를 refresh하고 render function을 호출한다.
+    // this.setState({ count: this.state.count +1 }); 이 방법은 추천하지않음. react가 똑똑해서 허락해준거뿐임
+    // this.setState(current => ({ count: current.count + 1 })); 이 방법이 state를 set 할때, react에서 외부의 상태에 의존하지 않는 베스트 방법
+    this.setState(current => ({ count: current.count + 1 }));
   }
   minus = () => {
-    console.log("minus");
+    this.setState(current => ({ count: current.count - 1 }))
   }
   render() {
     return (
