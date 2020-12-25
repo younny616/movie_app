c15e4abb25d82fa7d7505fd3670b44ff43d33367
yoonnayoung <younny616@gmail.com>
2020-12-26 02:34:23 +0900

    #6.1 Building the Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { HashRouter, Route } from "react-router-dom"; // react-router-dom 에 있는 HashRouter, Router를 사용한다.
+import Home from "./routes/Home";
+import About from "./routes/About";
 
 function App() {
-  return <span>something</span>
+  // about.js 로 들어가서 About component를 보여줘
+  // path 가 /, /about 이면 두개 component를 렌더링함, 이때 exact={true}를 사용하면, 해당하는 path일때만 해당 component를 렌더링 한다.
+  return <HashRouter>
+    <Route path="/" exact={true} component={Home} />
+    <Route path="/about" component={About} />
+  </HashRouter>
 }
 
 export default App;
