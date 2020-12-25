4e642356da1ec858c6320b178f8cbf44e8c73460
yoonnayoung <younny616@gmail.com>
2020-12-25 17:10:28 +0900

    #2.4 Protection with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,58 +1,60 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
 // data 시뮬레이션
 const foodILike = [
   {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
-  },
-  {
     id: 2,
     name: "Samgyeopsal",
     image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+    rating: 4.9
   },
   {
     id: 3,
     name: "Bibimbap",
     image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+    rating: 4.8
   },
   {
     id: 4,
     name: "Doncasu",
     image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+    rating: 5.5
   },
   {
     id: 5,
     name: "Kimbap",
     image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+    rating: 4.7
   }
 ];
 
 // Food(props) or Food(props.fav) or Food({fav})
-function Food({name, picture}) {
+function Food({name, picture, rating}) {
   return(
     <div>
-      <h2>{name}</h2>
+      <h2>I Like {name}</h2>
+      <div>{rating}/5.0</div>
       <img src={picture} alt={name} />
     </div>
   );
 }
 
-function renderFood(dish) {
-  return <Food key={dish.id} name={dish.name} picture={dish.image} />
+Food.prototype = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
 }
 
 function App() {
   return (
     <div>
       <h1>hello</h1>
-      {foodILike.map(renderFood)}
+      {foodILike.map(dish => <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />)}
     </div>
   );
 }
