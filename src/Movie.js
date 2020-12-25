092a918638a73e3c0a631db9db03617cb3d61af5
yoonnayoung <younny616@gmail.com>
2020-12-26 01:35:16 +0900

    #4.4 Styles Timelapse

--- src/Movie.js
+++ src/Movie.js
@@ -3,12 +3,12 @@ import ProtoTypes from "prop-types";
 import "./Movie.css";
 
 function Movie({id, year, title, summary, poster, genres}) {
-    return <div className="movies">
+    return <div className="movie">
         <img src={poster} alt={title} />
-        <div className="movies__data">
+        <div className="movie__data">
             <h3 className="movie__title">{title}</h3>
             <h5 className="movie__year">{year}</h5>
-            <ul className="genres">
+            <ul className="movie__genres">
                 {genres.map((genre, index) => (
                     <li key={index} className="genres__genre">{genre}</li>
                 ))}
