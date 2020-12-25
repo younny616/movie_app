ddaf6efcf107606bb1117a629d5d86c0ce9c3f33
yoonnayoung <younny616@gmail.com>
2020-12-26 03:21:35 +0900

    #6.4 Redirecting

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 import ProtoTypes from "prop-types";
 import "./Movie.css";
 
-function Movie({id, year, title, summary, poster, genres}) {
+function Movie({ id, year, title, summary, poster, genres }) {
     return (
         <Link
             to={{
-                pathname: "/movie-detail",
+                pathname: `/movie/${id}`,
                 state: {
                     year,
                     title,
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,8 +1,16 @@
 import React from "react";
 
-function Detail(props) {
-    console.log(props);
-    return <span>hello</span>
+class Detail extends React.Component{
+    componentDidMount() {
+        const { location, history } = this.props;
+        if(location.state === undefined) {
+            history.push("/");
+        }
+        console.log(location.state);
+    }
+    render() {
+        const { location } = this.props;
+    return <span>{location.state.title}</span>
+    }
 }
-
 export default Detail;
